5626abe67fe702e2effbd919dabdce6f8aa90ca0
Web Profits <webprofits@Troy-Daniels-15.local>
2015-09-24 17:29:01 +1000

    Initial commit

---
+++ background.js
@@ -0,0 +1,8 @@
+// Called when the user clicks on Tim
+chrome.browserAction.onClicked.addListener(function(tab) {
+  console.log('Loading some Tim Gro-tervation');
+  chrome.tabs.executeScript({
+     file: "inject.js"
+  });
+});
+
---
+++ inject.js
@@ -0,0 +1,37 @@
+var timURL = chrome.extension.getURL('timLarge.png');
+var timEl = document.querySelector('.tim-grotivate');
+
+if (timEl == null) {
+    //Try and include Tim on page
+    var div = document.createElement('div');
+    div.className = 'tim-grotivate';
+    div.innerHTML = '<div class="tim-image"><img id="timImage" src='+timURL+' /></div><div class="tim-quote" id="timsays">'+getInspiration()+'</div>';
+    document.body.appendChild(div);
+
+    //Does he live?
+    var tim = document.querySelector('.tim-grotivate');
+    tim.onerror = function() {
+        alert("No Tim Gro-tivation available");
+    };
+} else {
+    //Tim is already here. Change quote.
+    document.getElementById("timsays").value = getInspiration();
+}
+
+function getInspiration(){
+   var quote = "Tim says: ";
+   quote += randomQuote();  
+   return quote;
+}
+
+function randomQuote(){
+
+   var quotes =[
+                  "This is Gro-tivational!",
+                  "Do you like me now?",
+                  "Quote me, biatch!"
+               ];
+
+   //Return a random string
+   return quotes[Math.floor(Math.random()*quotes.length)];
+}
